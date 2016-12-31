9c51c743aa0a9c0698e3bcf5bc6217d5b9a8d19f
Shawn Knight <shawn.knight.work@gmail.com>
2016-12-30 23:32:29 -0600

    add @Test

--- src/test/java/com/knight/examples/tests/ScreenShotExamples.java
+++ src/test/java/com/knight/examples/tests/ScreenShotExamples.java
@@ -1,23 +1,40 @@
 package com.knight.examples.tests;
 
+import com.knight.examples.library.WebDriverHandler;
 import com.knight.examples.screenshot.ScreenShot;
 import com.knight.examples.screenshot.WebDriverScreenShot;
+import com.knight.examples.webdriver.Driver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 
 import java.util.function.Function;
 import java.util.function.Supplier;
 
-public class TakeScreenShotExamples {
+public class ScreenShotExamples {
 
     Function<String,Supplier<String>> applyBuildLocation = name -> () -> String.format("build/screenshots/%s.png",name);
     Function<String,Supplier<String>> applyOutLocation = name -> () -> String.format("out/screenshots/%s.png",name);
 
+    @Test
     public void exampleOne(){
         WebDriverScreenShot.save.accept("filename", ScreenShot.take);
     }
+    @Test
     public void exampleTwo(){
         WebDriverScreenShot.saveV2.accept(applyBuildLocation.apply("filename"));
     }
+    @Test
     public void exampleThree(){
         WebDriverScreenShot.saveV3.accept(applyOutLocation.apply("filename"));
+    }
+
+    @BeforeTest
+    public void setup() {
+        WebDriverHandler.setWebDriver(Driver.createInstance.apply("chrome"));
+    }
+    @AfterMethod
+    public void tearDown() {
+        Driver.quitInstance.accept(WebDriverHandler.getDriver());
     }
 }
